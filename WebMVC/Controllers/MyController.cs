5e1714b59e0ac67cfe24fde2be1d4cecdf7ac4bf
КахаровЖ <@N215-2>
2020-02-05 21:18:58 +0600

    uy

--- WebMVC/Controllers/MyController.cs
+++ WebMVC/Controllers/MyController.cs
@@ -1,19 +1,41 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebMVC.Models;
 
 namespace WebMVC.Controllers
 {
     public class MyController : Controller
     {
+        //[HttpGet]
+        //public ActionResult Index(string id1, string id2)
+        //{
+        //    ViewData["Result"] = id1 + " " + id2;
+        //    return View();
+        //    //string st = string.Format("{0} - {1}", "Hello", "world");
+        //}
+
         [HttpGet]
-        public ActionResult Index(string id1, string id2)
+        public ActionResult Index(string id)
         {
-            ViewData["Result"] = id1 + " " + id2;
             return View();
-            //string st = string.Format("{0} - {1}", "Hello", "world");
+        }
+        [HttpPost]
+        public List<string> PostTest(Country country)
+        {
+            List<string> countries = new List<JObject>();
+            for (int i = 0; i < 5; i++)
+            {
+                countries.Add(JsonConvert.SerializeObject(country));
+                
+            }
+            
+            IEnumerable<Country> countriesAsEnumerable = countries.AsEnumerable();
+            return countriesAsEnumerable;
         }
     }
 }
